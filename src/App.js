418b732f3194ff548f0b31024335b3bff12f104f
TS93 <tschram93@yahoo.com>
2022-01-18 23:32:10 -0500

    created context.js passing other components as props through themeprovider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import Introduction from './components/Introduction/Introduction';
@@ -7,15 +6,15 @@ import Toggle from './components/Toggle/Toggle';
 import Work from './components/Work/Work';
 
 const App = () => {
-	return( 
-  <div>
-    <Toggle />
-    <Introduction />
-    <About />
-    <Work />
-    <Contact />
-  </div>
-  )
+	return (
+		<div>
+				<Toggle />
+				<Introduction />
+				<About />
+				<Work />
+				<Contact />
+		</div>
+	);
 };
 
 export default App;
